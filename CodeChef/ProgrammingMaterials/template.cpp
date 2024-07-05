87f1e43e2fc4cf15f31cd12f2edcb19a2acc5b56
baranwalayush <ayush6262kr@gmail.com>
2024-07-05 15:33:44 +0530

    Added Programming Materials and my template

---
+++ CodeChef/ProgrammingMaterials/template.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+#define ll long long int
+#define endl "\n"
+#define pb push_back
+#define vi vector<int>
+#define vl vector<long long int>
+using namespace std;
+
+void solve(){
+    
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
