fcbcf1426ab9067e2ca75dc7c8bc6abd3bc16d57
baranwalayush <ayush6262kr@gmail.com>
2024-08-21 15:10:03 +0530

    Update template.cpp

--- ProgrammingMaterials/template.cpp
+++ ProgrammingMaterials/template.cpp
@@ -4,10 +4,18 @@
 #define pb push_back
 #define vi vector<int>
 #define vl vector<long long int>
+#define read(arr, high) for(int i=0;i<high;i++){cin>>arr[i];}
+#define loop(i, high) for(int i=0;i<high;i++)
 using namespace std;
 
 void solve(){
-    
+    int n;
+    cin>>n;
+    vi arr(n);
+    read(arr, n);
+    loop(i, n){
+        cout<<arr[i]<<" ";
+    }
 }
 
 int main(){
